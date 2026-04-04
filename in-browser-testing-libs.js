f88b8bdb47da41ef5c81a00e283406ba1b39b1b9
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-04 07:47:40 +0300

    chore: initial commit

--- in-browser-testing-libs.js
+++ in-browser-testing-libs.js
@@ -1,6 +1,21 @@
-import { Bytes } from '@z-base/bytecodec'
-import { Cryptosuite } from '@z-base/cryptosuite'
-import { ZKCredentials } from './dist/index.js'
-globalThis.Bytes = Bytes
-globalThis.Cryptosuite = Cryptosuite
-globalThis.ZKCredentials = ZKCredentials
+import { toBase64UrlString } from '@sovereignbase/bytecodec'
+import * as hardware from './dist/index.js'
+
+globalThis.hardware = hardware
+
+const nameInput = document.getElementById('name')
+
+const createButton = document.getElementById('create')
+
+const deriveButton = document.getElementById('derive')
+
+const resultOutput = document.getElementById('result')
+
+createButton.addEventListener('click', async () => {
+  void (await hardware.createDeviceBinding(nameInput.value ?? ''))
+})
+
+deriveButton.addEventListener('click', async () => {
+  const result = await hardware.deriveDeviceEntropy()
+  resultOutput.textContent = toBase64UrlString(result)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,94 @@
-export { ZKCredentials, type ZKCredential } from './ZKCredentials/class.js'
-export { type ZKCredentialErrorCode } from './ZKCredentials/errors.js'
+import { concat, fromString, toBufferSource } from '@sovereignbase/bytecodec'
+
+const timeout = 60_000
+const mediation: CredentialRequestOptions['mediation'] = 'required'
+const userVerification: AuthenticatorSelectionCriteria['userVerification'] =
+  'required'
+
+const prfInput1: BufferSource = toBufferSource(
+  fromString('INFO:ENTROPY_FROM_FIRST_PRF_RESULT')
+)
+const prfInput2: BufferSource = toBufferSource(
+  fromString('INFO:ENTROPY_FROM_SECOND_PRF_RESULT')
+)
+
+export async function createDeviceBinding(
+  usersDisplayName: string,
+  signal?: AbortSignal
+): Promise<boolean> {
+  const publicKey: PublicKeyCredentialCreationOptions = {
+    rp: { id: window.location.hostname, name: window.location.host },
+    user: {
+      id: crypto.getRandomValues(new Uint8Array(32)),
+      name: usersDisplayName,
+      displayName: usersDisplayName,
+    },
+    challenge: crypto.getRandomValues(new Uint8Array(32)),
+    pubKeyCredParams: [
+      { type: 'public-key', alg: -7 },
+      { type: 'public-key', alg: -257 },
+    ],
+    authenticatorSelection: {
+      authenticatorAttachment: 'platform',
+      residentKey: 'required',
+      userVerification: userVerification,
+    },
+    timeout: timeout,
+    attestation: 'none',
+    extensions: {
+      prf: {
+        eval: {
+          first: prfInput1,
+          second: prfInput2,
+        },
+      },
+    },
+  }
+
+  try {
+    await navigator.credentials.create({ publicKey, signal })
+    return true
+  } catch {
+    return false
+  }
+}
+
+export async function deriveDeviceEntropy(
+  signal?: AbortSignal
+): Promise<Uint8Array | false> {
+  try {
+    const credential = (await navigator.credentials.get({
+      publicKey: {
+        rpId: window.location.hostname,
+        challenge: crypto.getRandomValues(new Uint8Array(32)),
+        allowCredentials: [],
+        userVerification: userVerification,
+        timeout: timeout,
+        extensions: {
+          prf: {
+            eval: {
+              first: prfInput1,
+              second: prfInput2,
+            },
+          },
+        },
+      },
+      mediation: mediation,
+      signal,
+    })) as PublicKeyCredential
+    credential.response.clientDataJSON
+    const rawId = credential.rawId
+    if (rawId) {
+      const prf = credential.getClientExtensionResults().prf
+      if (prf && prf?.results) {
+        const { first, second } = prf.results
+        if (first && second) {
+          return concat([rawId, first, second])
+        }
+      }
+    }
+    return false
+  } catch {
+    return false
+  }
+}
